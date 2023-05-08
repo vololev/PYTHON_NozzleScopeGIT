6b4702dbf73bd87fd17d631aa862c506132ecd7c
v.levchenko <v.levchenko@aramis.tech>
2023-05-08 17:11:13 +0300

    Зміна бекапа (нагору)

--- !ADD_ON!/CreateBackup.bat
+++ !ADD_ON!/CreateBackup.bat
@@ -4,4 +4,4 @@ set datestr=%date:~6,4%%date:~3,2%%date:~0,2%
 REM 07:40:33,333 -> 074033
 set timestr=%time:~0,2%%time:~3,2%%time:~6,2%
 
-"C:\Program Files\7-Zip\7z.exe" a "..\Backup\NozzleAnalyzer_%datestr%-%timestr%.7z" @CreateBackup.txt
+"C:\Program Files\7-Zip\7z.exe" a "..\..\Backup\NozzleScope\NozzleScope_%datestr%-%timestr%.7z" @CreateBackup.txt
